155185f861f4536789cfb3c07db1294c49462947
吕向杰 <lvxiangjie@yangshipin.cn>
2022-03-10 16:18:23 +0800

    初始化

---
+++ src/Aw/Db/Reader/InvalidDatabaseException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Aw\Db\Reader;
+
+use Exception;
+
+/**
+ * This class should be thrown when unexpected data is found in the database.
+ */
+class InvalidDatabaseException extends Exception
+{
+}
